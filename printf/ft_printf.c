bff5e1b96b35aaaca307a57a8b5072f9e177dbbd
younghch <younghch@student.42seoul.kr>
2021-01-17 15:35:14 +0900

    update : count number of arguments

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,11 +6,43 @@
 /*   By: younghch <younghch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 15:50:49 by younghch          #+#    #+#             */
-/*   Updated: 2021/01/15 15:51:07 by younghch         ###   ########.fr       */
+/*   Updated: 2021/01/17 15:01:14 by younghch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-ft_printf(const char *, ...)
+#include "ft_printf.h"
+#include <stdio.h>
+
+int		get_arg_num(const char * format)
 {
-    
+	int flag;
+	int count;
+
+	count  = 0;
+	flag = 0;
+	while (*format)
+	{
+		if (flag && *format == '%')
+			flag = 0;
+		else if (*format == '%')
+			flag = 1;
+		else if (flag)
+		{
+			count++;
+			flag = 0;
+		}
+		format++;
+	}
+	return (count);
+}
+
+int		ft_printf(const char *format, ...)
+{
+	va_list		p_arg;
+	int			n_arg;
+
+	n_arg = get_arg_num(format);
+	va_start(p_arg, n_arg);
+	
+	return (0);
 }
---
+++ printf/ft_printf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: younghch <younghch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/17 13:59:35 by younghch          #+#    #+#             */
+/*   Updated: 2021/01/17 14:39:52 by younghch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define  FT_PRINTF_H
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "./libft/libft.h"
+
+#endif
